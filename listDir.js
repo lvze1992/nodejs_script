d9591001183cfc091e160db44a80bf003c31b1a1
ZAK <972010603@qq.com>
2018-03-30 11:12:35 +0800

    按照条件刷列表

--- listDir.js
+++ listDir.js
@@ -1,11 +1,13 @@
-const fs = require('fs');
+const fs = require('fs-extra');
+
 const path = require('path');
 
-const base = 'src/cards/+InvestBrain';
+const base = 'src/cards';
 const matchs = {
-  name: /config.js/,
-  code: /EventsTimeDistribution/g,
+  name: /^((?!example).)*config.js((?!example).)*$/, //path
+  code: /.\/imports/g, //code包含
 };
+const deleteBase = 'src/entry';
 const findFile = (base, matchs) => {
   // 层级遍历
   let r = [];
@@ -25,16 +27,40 @@ const findFile = (base, matchs) => {
   return r;
 };
 function match({ filepath }, { name, code }) {
-  const filename = filepath.split(path.sep).pop();
-  const lines = fs.readFileSync(filepath, 'utf8');
-  const result = !!name.exec(filename) && !!code.exec(lines);
+  // 必须这么做 不然文件会查不全  似乎是并发导致的
   name.lastIndex = 0;
   code.lastIndex = 0;
-  return result;
+  // const filename = filepath.split(path.sep).pop();
+  const lines = fs.readFileSync(filepath, 'utf8');
+  if (name && !name.exec(filepath)) {
+    return false;
+  }
+  if (code && !code.exec(lines)) {
+    return false;
+  }
+  return true;
 }
-
+function deleteFolder(path, boolean) {
+  fs.removeSync(path);
+  boolean && fs.mkdirsSync(path);
+}
+function writeFile(path, content) {
+  fs.writeFileSync(path, content, 'utf8');
+}
 const handle = () => {
-  const files = findFile(base, matchs);
-  console.log(files);
+  deleteFolder(deleteBase, true); //删除entry目录
+  const files = findFile(base, matchs); //所有的configs
+  // writeFile('framework/scripts/list.js', files.join('\n'));
+  // console.log(files);
+  // return;
+  files.forEach((i) => {
+    console.log(i);
+    const exist = /const name = '([^']+)'/.exec(fs.readFileSync(i));
+    const filename = exist[1];
+    const content = `export { default } from '${i
+      .replace('src/', '../')
+      .replace('.js', '')}';`;
+    writeFile(`src/entry/${filename}.js`, content);
+  });
 };
 handle();
